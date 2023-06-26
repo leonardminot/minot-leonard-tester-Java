15b13333b8c4eb9bb12b56911600f089e5eaf9ba
leonard.minot <leonard.minot@gadz.org>
2023-06-26 17:12:20 +0200

    Integration Tests completed

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,11 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -13,6 +15,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,17 +53,56 @@ public class ParkingDataBaseIT {
 
     @Test
     public void testParkingACar(){
+        // Given
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+
+        // When
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        //TODO: verify test with Alex ??
+        //  L'assert doit il être une requête à une base de données ?
+
+        // Then
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        assertThat(ticket.getInTime()).isNotNull();
     }
 
     @Test
     public void testParkingLotExit(){
+        // Given
         testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        dataBasePrepareService.removeOneHourFormInTimeTicket(ticket); // Remove 1 hour to be able to compute the fare
+
+        // When
         parkingService.processExitingVehicle();
+        ticket = ticketDAO.getTicket("ABCDEF");
+
+        // Then
         //TODO: check that the fare generated and out time are populated correctly in the database
+        assertThat(ticket.getOutTime()).isNotNull();
+        assertThat(ticket.getPrice()).isEqualTo((ticket.getOutTime().getTime() - ticket.getInTime().getTime()) / (double)(1000 * 60 * 60) * Fare.CAR_RATE_PER_HOUR);
+    }
+
+    @Test
+    public void testParkingLotExitRecurringUser() {
+        // Given
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        parkingService.processIncomingVehicle(); // First entrance
+        parkingService.processExitingVehicle(); // First Exit
+        parkingService.processIncomingVehicle(); // Second entrance
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        dataBasePrepareService.removeOneHourFormInTimeTicket(ticket); // Remove 1 hour to be able to compute the fare
+
+        // When
+        parkingService.processExitingVehicle(); // Second exit : should have 5% discount
+        ticket = ticketDAO.getTicket("ABCDEF");
+
+        // Then
+        double expectedPrice = (ticket.getOutTime().getTime() - ticket.getInTime().getTime() )/ (double) (1000 * 60 * 60) * Fare.CAR_RATE_PER_HOUR * 0.95;
+        double expectedRoundedPrice = (double)Math.round((double) expectedPrice * 1000) / 1000;
+        assertThat(ticket.getPrice()).isEqualTo(expectedRoundedPrice);
+
     }
 
 }
--- src/test/java/com/parkit/parkingsystem/integration/service/DataBasePrepareService.java
+++ src/test/java/com/parkit/parkingsystem/integration/service/DataBasePrepareService.java
@@ -1,8 +1,12 @@
 package com.parkit.parkingsystem.integration.service;
 
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
+import com.parkit.parkingsystem.model.Ticket;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
 
 public class DataBasePrepareService {
 
@@ -38,6 +42,21 @@ public class DataBasePrepareService {
             dataBaseTestConfig.closeConnection(connection);
         }
 
+    }
+
+    public void removeOneHourFormInTimeTicket(Ticket ticket) {
+        Connection connection = null;
+        try {
+            connection = dataBaseTestConfig.getConnection();
+            PreparedStatement ps = connection.prepareStatement("update ticket set IN_TIME = ? where ID = ?");
+            ps.setTimestamp(1, new Timestamp(ticket.getInTime().getTime() - 60 * 60 * 1000));
+            ps.setInt(2,ticket.getId());
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            dataBaseTestConfig.closeConnection(connection);
+        }
     }
 
 
