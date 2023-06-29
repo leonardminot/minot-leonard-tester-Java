44c5778c42a54d65a2aee36a42fdf520712a6ad1
leonard.minot <leonard.minot@gadz.org>
2023-06-29 10:58:49 +0200

    SpotBugs' improvement : PreparedStatement was not closed in TicketDAO::updateTicket

--- src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -75,9 +75,10 @@ public class TicketDAO {
 
     public boolean updateTicket(Ticket ticket) {
         Connection con = null;
+        PreparedStatement ps = null;
         try {
             con = dataBaseConfig.getConnection();
-            PreparedStatement ps = con.prepareStatement(DBConstants.UPDATE_TICKET);
+            ps = con.prepareStatement(DBConstants.UPDATE_TICKET);
             ps.setDouble(1, ticket.getPrice());
             ps.setTimestamp(2, new Timestamp(ticket.getOutTime().getTime()));
             ps.setInt(3,ticket.getId());
@@ -87,6 +88,7 @@ public class TicketDAO {
             logger.error("Error saving ticket info",ex);
         }finally {
             dataBaseConfig.closeConnection(con);
+            dataBaseConfig.closePreparedStatement(ps);
         }
         return false;
     }
