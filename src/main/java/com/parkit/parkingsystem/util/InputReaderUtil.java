f91001f82b5c640cff7d74e4d367c552478f8135
leonard.minot <leonard.minot@gadz.org>
2023-06-29 12:09:47 +0200

    SpotBugs' improvement : InputReaderUtil, Scanner defined with UTF_8

--- src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
+++ src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
@@ -3,11 +3,12 @@ package com.parkit.parkingsystem.util;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class InputReaderUtil {
 
-    private final static Scanner scan = new Scanner(System.in);
+    private final static Scanner scan = new Scanner(System.in, StandardCharsets.UTF_8);
     private static final Logger logger = LogManager.getLogger("InputReaderUtil");
 
     public int readSelection() {
